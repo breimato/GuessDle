a7bfb4a644efc6009b5f8ff5f4f50f930771313d
BreiMato <breimatom@gmail.com>
2025-05-13 12:13:49 +0200

    Redirect al login en la URL base

--- GuessDle/urls.py
+++ GuessDle/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda request: redirect('login')),
     path('admin/', admin.site.urls),
     path('home/', include('apps.accounts.urls')),
     path('accounts/', include('apps.accounts.urls')),
