f77e92d557a17e638578946caa74803ee6a21d73
BreiMato <breimatom@gmail.com>
2025-05-22 15:29:06 +0200

    Relocate de las URLs de password recovery

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
-from django.views.generic.base import RedirectView
-from django.urls import reverse_lazy
 from .views import dashboard_view, register_view
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', dashboard_view, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', register_view, name='register'),
+    path("password_reset/", auth_views.PasswordResetView.as_view(
+        template_name="accounts/password_reset_form.html"
+    ), name="password_reset"),
+
+    path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(
+        template_name="accounts/password_reset_done.html"
+    ), name="password_reset_done"),
+
+    path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(
+        template_name="accounts/password_reset_confirm.html"
+    ), name="password_reset_confirm"),
+
+    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(
+        template_name="accounts/password_reset_complete.html"
+    ), name="password_reset_complete"),
 
 ]
