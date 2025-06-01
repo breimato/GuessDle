c8c28cf1397da3d2e24e66c2f5ea517fe69778b8
BreiMato <breimatom@gmail.com>
2025-06-01 23:10:11 +0200

    borrada la linea de las migraciones

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,6 @@ cron
 # Lanza el bot de Discord en segundo plano
 python bot_discord/bot.py &
 
-echo "🔍 Verificando si hay migraciones pendientes..."
-python manage.py migrate --noinput
 
 # Lanza el servidor Django
 python manage.py runserver 0.0.0.0:8000
