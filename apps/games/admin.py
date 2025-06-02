a2310d5720eafe41d0ad0735d852297a8e3853a5
borrageiros <adrianborrageirosmourelos@gmail.com>
2025-06-02 22:25:17 +0200

    feat: add game color and little discord bot improves

--- apps/games/admin.py
+++ apps/games/admin.py
@@ -4,10 +4,21 @@ from .models import GameItem
 
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
-    list_display = ('name', 'slug', 'active', 'created_at')
-    search_fields = ('name', 'slug')
+    list_display = ('name', 'slug', 'color', 'active', 'created_at')
+    search_fields = ('name', 'slug', 'color')
     list_filter = ('active',)
     prepopulated_fields = {"slug": ("name",)}
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'slug', 'description', 'active')
+        }),
+        ('Visuals', {
+            'fields': ('color', 'icon_image', 'background_image')
+        }),
+        ('API Configuration', {
+            'fields': ('data_source_url', 'field_mapping', 'defaults', 'attributes', 'numeric_fields', 'audio_file')
+        }),
+    )
 
 
 @admin.register(GameItem)
